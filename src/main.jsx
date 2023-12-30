c752ec471c785d3a16b3d4763cb2021980653b19
Mariana da Silva <dasilval.mariana@gmail.com>
2023-12-30 15:31:15 -0300

    🦄 create account screen

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import { ThemeProvider } from "styled-components"
 
 import GlobalStyles from "./styles/global"
 import themes from "./styles/theme"
-import { SignIn } from './pages/SignIn'
+import { SignUp } from './pages/SignUp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={themes}>
       <GlobalStyles />
-      <SignIn />
+      <SignUp />
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,41 @@
+import { Container, Form, Background } from './styled';
+import { Input } from '../../components/Input';
+import { FiMail, FiLock, FiUser } from 'react-icons/fi';
+
+import { Button } from '../../components/Button';
+
+export function SignUp() {
+    return (
+        <Container>
+            <Background />
+            <Form>
+                <h1>Vite Notes</h1>
+                <p>Aplicação para Salvar e Gerenciar seus Links úteis.</p>
+
+                <h2>Crie sua Conta</h2>
+
+                <Input 
+                    placeholder="Nome"
+                    type="text"
+                    icon={FiUser}
+                />
+
+                <Input 
+                    placeholder="E-mail"
+                    type="text"
+                    icon={FiMail}
+                />
+
+                <Input 
+                    placeholder="Senha"
+                    type="password"
+                    icon={FiLock}
+                />
+
+                <Button title="Cadastrar" />
+
+                <a href="#">Voltar para o Login</a>
+            </Form>
+        </Container>
+    )
+}
